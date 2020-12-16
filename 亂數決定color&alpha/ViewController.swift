0e0b7a2b9bbdf3e0ba624a5fa15b08d4ebf85327
haoyun317 <haoyun0317@gmail.com>
2020-12-16 14:10:55 +0800

    Initial Commit

---
+++ 亂數決定color&alpha/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  亂數決定color&alpha
+//
+//  Created by  謝皓昀 on 2020/12/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
